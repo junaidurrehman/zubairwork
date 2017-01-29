432e9b36db938e969e021816865ad201b7776ce7
derfokn80976 <derfokn80976_3232kil3_i@yahoo.com>
2017-01-29 21:31:31 +0500

    css

--- rs16/index.php
+++ rs16/index.php
@@ -17,7 +17,7 @@ and open the template in the editor.
     <body>
         <div id="navigation-bar">
             <nav class="navbar navbar-default">
-                <div class="container-fluid">
+                <div class="container">
                   <!-- Brand and toggle get grouped for better mobile display -->
                   <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -42,7 +42,7 @@ and open the template in the editor.
                       
                     </ul>
                   </div><!-- /.navbar-collapse -->
-                </div><!-- /.container-fluid -->
+                
               </nav>
         </div>
         
@@ -91,10 +91,8 @@ and open the template in the editor.
                 </a>
               </div>
         </div>
-        <div id="third">
-            
-        </div>
-        
+       
+    </div>
         <?php
         // put your code here
         ?>
